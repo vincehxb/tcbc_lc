f427275a3372f6f1a7d1e802c505fe7cd55a2f41
vincent <vincenthouxb@gmail.com>
2021-05-26 10:11:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import os
 from flask import Flask
 app = Flask(__name__)
+def get_today_top_question():
+  all_res=[]
+  with open("easy_top_qustion.txt",'r') as fp:
+    for line in fp:
+      q=line.strip()
+      all_res.append(q)
+  random.shuffle(all_res)
+  return all_res
+
 @app.route('/')
 def hello_world():
-  return 'Yo quiero mi camiseta!! por favor'
+  all_questions_rand=get_today_top_question()
+  return "Todays LeetCode Question: {}".format(all_questions_rand[0])
+  #return 'Yo quiero mi camiseta!! por favor'
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0', port=8080)
